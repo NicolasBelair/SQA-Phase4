a4b447b51b2cd4a80024888203f3a6689ea0f682
Kevin Chandra <44078398+Sclatch@users.noreply.github.com>
2021-04-05 14:55:29 -0400

    Junit

---
+++ JunitTest/src/prototype/BackendTest.java
@@ -0,0 +1,14 @@
+package prototype;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BackendTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
