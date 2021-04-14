06d3c3be45a5cc30eb042cb6abf9e303b06972bb
jimkaran <jimkaran@users.noreply.github.com>
2021-04-14 15:55:35 -0400

    daily script try #1

---
+++ daily.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+{
+cd tests/daily 
+    counter=1 #counter variable for incrementing the actual output files
+    for c in tests/daily/*.inp ; do # loop through each file in the directory
+        echo "Running Daily Transactions"
+        ../../bank-atm tests/daily/output/transaction_daily${counter}.atf # run the tests, and store the actual outputs in an output folder
+        counter=$((counter+1)) #increment the counter variable
+        done
+}
