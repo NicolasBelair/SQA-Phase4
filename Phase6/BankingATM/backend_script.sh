0943cc1fdd86df5e79f79d208fdbb702fa76799c
jimkaran <jimkaran@users.noreply.github.com>
2021-04-16 21:57:20 -0400

    fixed

--- Phase6/BankingATM/backend_script.sh
+++ Phase6/BankingATM/backend_script.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#filepaths for the backend input data
-master_accounts_filepath="C:\Users\jim\Documents\GitHub\SQA-Phase4\Phase6\BankingATM\masterAccount.txt"
-merged_transactions_filepath="C:\Users\jim\Documents\GitHub\SQA-Phase4\Phase6\BankingATM\transactions.etf"
-
 #run backend with given datapaths
-java Backend ${master_accounts_filepath} ${merged_transactions_filepath}
+java Backend masterAccount.txt transactions.etf
